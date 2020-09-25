0675c796d9aaba75948c7bb68cf9d8f2a79393cc
ENyamdorj <ENyamdorj@github.com>
2020-09-25 13:52:41 +0800

    aa

--- monospharma/settings/development.py
+++ monospharma/settings/development.py
@@ -2,16 +2,3 @@ from .common import *
 
 
 # DEBUG = False
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'emholboo',
-        'USER': 'emholboouser',
-        'PASSWORD': 'emholboopassword',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
