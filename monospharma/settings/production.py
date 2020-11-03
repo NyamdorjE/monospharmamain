845e34f0565f5f52852d88cfae7c9704fcd24fdc
Enyamdorj <nyamdorj@bumanit.mn>
2020-11-03 09:35:46 +0800

    fix bug

--- monospharma/settings/production.py
+++ monospharma/settings/production.py
@@ -7,29 +7,29 @@ Production settings
 from .common import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['10.0.0.153', 'localhost']
+ALLOWED_HOSTS = ["10.0.0.153", "localhost"]
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mnpharma',
-        'USER': 'mnpharmauser',
-        'PASSWORD': 'mnpharmauser',
-        'HOST': 'localhost',
-        'PORT': '5432'
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mnpharma",
+        "USER": "mnpharmauser",
+        "PASSWORD": "mnpharmauser",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "file": {
-            "level": "DEBUG",
-            "class": "logging.FileHandler",
-            "filename": "debug.log",
-        },
-    },
-    "loggers": {
-        "django": {"handlers": ["file"], "level": "DEBUG", "propagate": True, },
-    },
-}
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "handlers": {
+#         "file": {
+#             "level": "DEBUG",
+#             "class": "logging.FileHandler",
+#             "filename": "debug.log",
+#         },
+#     },
+#     "loggers": {
+#         "django": {"handlers": ["file"], "level": "DEBUG", "propagate": True, },
+#     },
+# }
