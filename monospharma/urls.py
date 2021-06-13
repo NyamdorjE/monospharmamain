a24d661345f6858897f1ce26d19dff70dc3194e9
root <root@webserver-153.org.monos.mn>
2021-06-13 15:38:43 +0800

    urls from production

--- monospharma/urls.py
+++ monospharma/urls.py
@@ -58,7 +58,7 @@ urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
     re_path(r"^ckeditor/", include("ckeditor_uploader.urls")),
     # url(r'^contact/', include('contact.urls', namespace='contact')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #if settings.DEBUG:
  #   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
