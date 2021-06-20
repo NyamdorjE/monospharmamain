7f6a1f88184bbb5295242a2adad5e199ce507e6e
Enyamdorj <nyamdorj@bumanit.mn>
2021-06-20 21:48:31 +0800

    admin.py product

--- src/product/admin.py
+++ src/product/admin.py
@@ -51,16 +51,47 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(ProductCategory)
 class ProductCategoryAdmin(DraggableMPTTAdmin):
     """
-    Product Category
+    Product ProductCategory
     """
 
     list_display_links = ("indented_title",)
-    list_display = ["tree_actions", "indented_title"]
+    list_display = [
+        "tree_actions",
+        "indented_title",
+        "related_products_count",
+        "related_products_cumulative_count",
+    ]
     mptt_indent_field = "name"
     prepopulated_fields = {"slug": ("name",)}
     search_fields = [
         "name",
     ]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        # Add cumulative product count
+        qs = ProductCategory.objects.add_related_count(
+            qs, Product, "categories", "products_cumulative_count", cumulative=True
+        )
+
+        # Add non cumulative product count
+        qs = ProductCategory.objects.add_related_count(
+            qs, Product, "categories", "products_count", cumulative=False
+        )
+        return qs
+
+    def related_products_count(self, instance):
+        return instance.products_count
+
+    related_products_count.short_description = (
+        "Related products (for this specific category)"
+    )
+
+    def related_products_cumulative_count(self, instance):
+        return instance.products_cumulative_count
+
+    related_products_cumulative_count.short_description = "Related products (in tree)"
 
 
 # admin.site.register(Classification)
